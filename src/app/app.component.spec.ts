978976811e6cfb46419d7df6343986b19b5f7636
Andrey Marchenko <marchenkoandrey1993pavlovich@gmail.com>
2025-01-13 23:33:44 +0100

    created clean project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,23 +7,4 @@ describe('AppComponent', () => {
       imports: [AppComponent],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'rabobank-transactions' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('rabobank-transactions');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rabobank-transactions');
-  });
 });
