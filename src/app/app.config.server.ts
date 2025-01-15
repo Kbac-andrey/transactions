db33a4860bb1062e806da8607d252779d238f8f8
Andrey Marchenko <marchenkoandrey1993pavlovich@gmail.com>
2025-01-15 19:28:30 +0100

    fixed warnings and errors

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import {TransactionDetailsGuard}                     from './transactions/guard/transaction-details.guard';
 
 const serverConfig: ApplicationConfig = {
   providers: [
